f92fac563826e0c1921a17fb24b55b68b1bfa63f
Xeltica <xeltica@gmail.com>
2020-09-21 08:14:08 +0900

    1週間経過したリレー小説チャンネルを終わらせる機能

--- src/workers/finishNovel.ts
+++ src/workers/finishNovel.ts
@@ -1,8 +1,23 @@
-import { Message, Client, TextChannel } from 'discord.js';
-import { getNovelChannel, getSiritoriChannel } from '../misc/env';
-import { fetchAllMessages } from '../misc/fetchAllMessages';
+import dayjs from 'dayjs';
+import { Client, TextChannel } from 'discord.js';
+import { getNovelChannel } from '../misc/env';
 import { fetchLastMessage } from '../misc/fetchLastMessage';
 
 export default async (cli: Client): Promise<void> => {
-      
+    const id = getNovelChannel();
+    if (!id) return;
+
+    try {
+        const ch = await cli.channels.fetch(id);
+        if (!(ch instanceof TextChannel)) return;
+
+        const last = (await fetchLastMessage(ch));
+        if (!last) return;
+
+        if (last.author.id !== cli.user?.id && dayjs().diff(dayjs(last.createdAt), 'day') >= 7) {
+            await ch.send({ content: '〜終〜' });
+        }
+    } catch (e: unknown) {
+        console.error(e);
+    }
 };
